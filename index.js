85138405bfb1b23578f601aee3a0b6bc925f3e5f
Seth Mwebi <sethmwebi27@gmail.com>
2024-06-29 10:35:44 +0300

    writes code on looping

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const writeCards = (arr, message) => {
+  const messages = [];
+  for (let i = 0; i < arr.length; i++) {
+    messages.push(`Thank you, ${arr[i]}, for the wonderful ${message} gift!`);
+  }
+  return messages;
+};
+
+const countDown = (value) => {
+  while (value >= 0) {
+    console.log(value);
+    value--;
+  }
+};
